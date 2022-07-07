a93fae11472525bcd4181248b0ff4391903fcee5
Tomas Payer <tomaspayer18@gmail.com>
2022-07-06 22:30:43 -0300

    changes

---
+++ computo.js
@@ -0,0 +1,15 @@
+// Child Process
+
+
+
+process.on('message', msg => {
+    console.log(`Mensaje del proceso padre: ${msg}`);
+
+    let sum = 0;
+
+    for (let index = 0; index < 6e9; index++) {
+        sum += index
+    }
+
+    process.send(sum)
+})
